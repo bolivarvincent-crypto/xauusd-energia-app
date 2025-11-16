52840551483d152065d4dbca411da1b13318b57b
bolivarvincent-crypto <bolivarvincent@gmail.com>
2025-11-16 13:07:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,14 @@ def home():
 @app.route('/precio')
 def precio():
     try:
-        url = "https://data-asg.goldprice.org/dbXRates/USD"
+        url = "https://forex-data-feed.swissquote.com/public-quotes/bboquotes/instrument/XAU/USD"
         res = requests.get(url, timeout=10).json()
-        precio = res["items"][0]["xauPrice"]
+
+        # Tomamos el primer broker y su primer perfil de precios
+        precio = res[0]["spreadProfilePrices"][0]["bid"]
+
         return jsonify({"gold_price_usd": precio})
+
     except Exception as e:
         return jsonify({"error": str(e), "gold_price_usd": None})
 
